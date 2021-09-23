3fefd9cae812a5f71b6ae39cb064c1b634c446ba
Hkurra <er.harshkurra@gmail.com>
2021-09-24 01:28:25 +0530

    Integrated the new getFlagURL from flag microservices

--- apps/trainingbase/trainingbase-cms/src/main/frontend/TrainingWidgetFlag/src/services/dataService.js
+++ apps/trainingbase/trainingbase-cms/src/main/frontend/TrainingWidgetFlag/src/services/dataService.js
@@ -1,10 +1,32 @@
 import COUNTRY_CODE from "./countryCodes";
 
-export const getCountryFlag = function (countryCode) {
-  return `https://www.countryflags.io/${countryCode}/flat/64.png`;
+export const getFlagInfo = function (HttpClient, countryCode) {
+  let config = {
+    method: "get",
+    url: "/bin/mvc.do/genericappkar/v1/app/" + countryCode
+  };
+  return new Promise((resolve, reject) => {
+    HttpClient.request(config)
+      .then((res) => {
+        const response = res.data;
+        console.log(response, "response");
+        if (!!response) {
+          resolve(response);
+          return;
+        }
+        reject("not found");
+      })
+      .catch(function (error) {
+        console.log(error, "error");
+        reject(error);
+      });
+  });
 };
 
-export const getRandomCountry = function () {
+export const getRandomCountry = function (HttpClient) {
   const randomIndex = Math.floor(Math.random() * COUNTRY_CODE.length - 1);
-  return COUNTRY_CODE[randomIndex];
+  const country = COUNTRY_CODE[randomIndex];
+  return getFlagInfo(HttpClient, country.Code).then((data) => {
+    return { ...data, ...country };
+  });
 };
--- apps/trainingbase/trainingbase-cms/src/main/frontend/TrainingWidgetFlag/src/widget.js
+++ apps/trainingbase/trainingbase-cms/src/main/frontend/TrainingWidgetFlag/src/widget.js
@@ -1,26 +1,35 @@
 import React from "react";
 import { withRootHoc } from "omni-widget";
-import { getRandomCountry, getCountryFlag } from "./services/dataService";
+import { getRandomCountry } from "./services/dataService";
 import { useSelector } from "react-redux";
 
 function Widget() {
-  const I18nProvider = useSelector(
-    (store) => store.widget.services.I18nProvider
-  );
+  const { I18nProvider, HttpClient } = useSelector((store) => {
+    return {
+      I18nProvider: store.widget.services.I18nProvider,
+      HttpClient: store.widget.services.HttpClient
+    };
+  });
 
-  const [currentCountry, setCurrentCountry] = React.useState(
-    getRandomCountry()
-  );
+  const [currentCountry, setCurrentCountry] = React.useState(null);
   React.useEffect(() => {
     setInterval(() => {
-      setCurrentCountry(getRandomCountry());
+      getRandomCountry(HttpClient).then((countryInfo) => {
+        setCurrentCountry(countryInfo);
+      });
     }, 5000);
   }, []);
   return (
     <div style={ROOT_STYLE}>
       <span>{I18nProvider.Labeli18n("world_flag")}</span>
-      <img src={getCountryFlag(currentCountry.Code)} />
-      <span>{currentCountry.Name}</span>
+      {currentCountry ? (
+        <>
+          <img style={FLAG_IMAGE_STYLE} src={currentCountry.urlFlag} />
+          <span>{currentCountry.name}</span>
+        </>
+      ) : (
+        I18nProvider.Labeli18n("loading")
+      )}
     </div>
   );
 }
@@ -31,7 +40,13 @@ const ROOT_STYLE = {
   display: "flex",
   height: "100%",
   alignItems: "center",
-  justifyContent: "center",
+  justifyContent: "space-around",
   position: "relative",
-  flexDirection: "column"
+  flexDirection: "column",
+  padding: 10
+};
+
+const FLAG_IMAGE_STYLE = {
+  width: "100px",
+  height: "auto"
 };
